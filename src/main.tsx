8624d2c9a35d59d02b6714af1a7c202835277554
Yauheni Kadyrau <7077634@gmail.com>
2024-07-05 11:29:53 +0300

    feat: add routing for pages

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { RouterProvider } from 'react-router-dom';
+
+import { rootRouter } from '@/router';
 
 import App from './App';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
 	<React.StrictMode>
-		<App />
+		<RouterProvider router={rootRouter}>
+			<App />
+		</RouterProvider>
 	</React.StrictMode>
 );
---
+++ src/router/index.tsx
@@ -0,0 +1,21 @@
+import { createBrowserRouter, createRoutesFromElements, Route } from 'react-router-dom';
+import { SuspenseLayoutForAuthBlock, SuspenseLayoutForMainContent } from '@components/Layouts';
+
+import { Paths } from '@/constants/routerPaths';
+
+export const rootRouter = createBrowserRouter(
+	createRoutesFromElements(
+		<Route path={Paths.HOME} errorElement="Error">
+			<Route element={<SuspenseLayoutForAuthBlock />}>
+				<Route index element="Home" />
+				<Route path={Paths.LOGIN} element="Login" />
+				<Route path={Paths.REGISTRATION} element="Register" />
+			</Route>
+			<Route element={<SuspenseLayoutForMainContent />}>
+				<Route path={Paths.FEED} element="Feed" />
+				<Route path={Paths.PROFILE} element="Profile" />
+			</Route>
+			<Route path={Paths.NOT_FOUND} element="Not found" />
+		</Route>
+	)
+);
