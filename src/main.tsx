55eedb24a969118be4c6d698ef69d319a8636443
Yauheni Kadyrau <7077634@gmail.com>
2024-07-05 13:26:55 +0300

    fix: fix router provider

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { RouterProvider } from 'react-router-dom';
-
-import { rootRouter } from '@/router';
 
 import App from './App';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
 	<React.StrictMode>
-		<RouterProvider router={rootRouter}>
-			<App />
-		</RouterProvider>
+		<App />
 	</React.StrictMode>
 );
